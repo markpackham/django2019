77b52904de2fb52d301b676aa8a4df5fdd86015d
Mark Packham <markpackham1@gmail.com>
2019-09-10 13:23:30 +0100

    working on the views for results & votes https://youtu.be/e1IyzVyrLSU?t=3471

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+admin.site.site_header = "Pollster Admin"
+admin.site.site_title = "Pollster Admin Area"
+admin.site.index_title = "Welcome to the Pollster"
+
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
